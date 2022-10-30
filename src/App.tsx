96b05a96a910866d1310dc4dbb580948eafe8c47
Ali-consciousroad <ali_dindar@live.be>
2022-10-30 17:20:45 +0100

    Observables: Use MobX observable instead of setState()

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,38 @@ import TodoInput from "./Todo/TodoInput";
 import TodoList from "./Todo/TodoList";
 import styles from "./App.module.css";
 import { useState } from "react";
+import { observable } from "mobx";
+import { observer } from "mobx-react-lite";
 
 const App = () => {
-  const [todosVisible, setTodosVisible] = useState(true);
-  const handleClick = () => setTodosVisible((state) => !state);
+  // Functions used to toggle the to do list when clicking on the " + " / " - " button
+  // Use MobX instead of set state
+  const [appUI] = useState(() => 
+      observable({
+        todosVisible: true,
+        toggleTodoVisibility() {
+          this.todosVisible = !this.todosVisible;
+        },
+      })
+    );
+  // Change the state of the useState hook based on  click 
+  const handleClick = () => appUI.toggleTodoVisibility();
+
   return (
     <div className="App">
       {/* Props removed to avoid prop drilling, useStore() hook is used instead */}
       <TodoInput />
       <div className={styles["todo-list-wrapper"]}>
         <h2 onClick={handleClick}>
-          <span>{todosVisible ? "-" : "+"}</span>
+          {/* Conditional rendering of the text used as a button */}
+          <span>{appUI.todosVisible ? "-" : "+"}</span>
           Todos
         </h2>
-        <TodoList />
+        {/* Render conditionally the list */}
+        {appUI.todosVisible && <TodoList />}
       </div>
     </div>
   );
 };
 
-export default App;
+export default observer(App);
